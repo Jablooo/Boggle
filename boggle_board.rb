f3dce0e85b0e81fbae4c95da631c2a8e9030f9a7
Jon Ablondi <jonnyablondi@hotmail.com>
2017-09-12 11:40:08 +1000

    board converted to one object

--- boggle_board.rb
+++ boggle_board.rb
@@ -5,9 +5,7 @@ class BoggleBoard
 
   # def shake!
   # end
-  #
-  # # Defining to_s on an object controls how the object is
-  # # represented as a string, e.g., when you pass it to puts
+
   def to_s
     puts "omg what is this? try printing me."
     puts @board
