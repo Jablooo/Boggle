aa5c60abbcc17ccb6edc75be81f1443628d2f43b
Jon Ablondi <jonnyablondi@hotmail.com>
2017-09-12 11:34:05 +1000

    board.to_s working

--- boggle_board.rb
+++ boggle_board.rb
@@ -1,20 +1,19 @@
 class BoggleBoard
-  def initialize(board)
-    @board = board
+  def initialize
+    @board = ["____","____","____","____"]
   end
-
-  attr_accessor :board
 
   # def shake!
   # end
   #
   # # Defining to_s on an object controls how the object is
   # # represented as a string, e.g., when you pass it to puts
-  # def to_s
-  #   "omg what is this? try printing me."
-  # end
+  def to_s
+    puts "omg what is this? try printing me."
+    puts @board
+  end
 end
 
-board = BoggleBoard.new(["____","____","____","____"])
+board = BoggleBoard.new
 
-puts board.board
+puts board.to_s
